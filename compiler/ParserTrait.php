5adcd768d1f27e41bf17e0fc997718719e59a326
Haruhito Yamada <yamada@iknow.dev>
2025-11-03 08:37:12 +0900

    update 引数のエスケープに対応

--- compiler/ParserTrait.php
+++ compiler/ParserTrait.php
@@ -128,6 +128,13 @@ trait ParserTrait
             return;
         }
 
+        if (strpos($attrName, 'data-') !== false && in_array($attrName, [
+            BLOCS_DATA_VAL, BLOCS_DATA_PREFIX, BLOCS_DATA_POSTFIX, BLOCS_DATA_CONVERT, BLOCS_DATA_LANG,
+            BLOCS_DATA_ENDIF, BLOCS_DATA_ENDUNLESS,
+        ])) {
+            $attrValue = stripslashes($attrValue);
+        }
+
         $attrList[$attrName] = $attrValue;
     }
 
