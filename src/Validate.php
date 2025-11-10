497bb3f2e39cbcbecf44adbbc73bce018a50f758
Haruhito Yamada <yamada@iknow.dev>
2025-11-11 07:56:31 +0900

    refactor

--- src/Validate.php
+++ src/Validate.php
@@ -33,7 +33,7 @@ class Validate
         $validateMessage = [];
         foreach ($configMessage as $formName => $messageList) {
             foreach ($messageList as $dataValidate => $message) {
-                $validateMessage[$formName.'.'.$dataValidate] = Lang::get($message);
+                $validateMessage[$formName.'.'.$dataValidate] = __($message);
             }
         }
 
@@ -79,7 +79,7 @@ class Validate
         if (isset($config['upload'][$formName]['message'])) {
             $messageList = $config['upload'][$formName]['message'];
             foreach ($messageList as $dataValidate => $message) {
-                $uploadMessage['upload.'.$dataValidate] = Lang::get($message);
+                $uploadMessage['upload.'.$dataValidate] = __($message);
             }
         }
 
